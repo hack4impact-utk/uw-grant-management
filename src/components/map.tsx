6c4d89a3f1b73ff631116e20f11d38249eb90cdf
kvnguo <kevinguo1011@gmail.com>
2023-11-10 14:17:36 -0500

    Added map component

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,16 +1,32 @@
 'use client';
-
 import './mapstyles.css';
 import 'leaflet/dist/leaflet.css';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { LatLngTuple, Icon } from 'leaflet';
+
 function Map() {
+  const knoxvillePosition: LatLngTuple = [35.9606, -83.9207]; // Latitude and Longitude of Knoxville, TN
+
+  const redMarker = new Icon({
+    iconUrl:
+      'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+  });
+
   return (
-    <MapContainer center={[35.9606, -83.9207]} zoom={13}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-    </MapContainer>
+    <div>
+      <MapContainer center={knoxvillePosition} zoom={13}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={knoxvillePosition} icon={redMarker}>
+          <Popup>Knoxville, 37920</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   );
 }
 
