6bb9de3c35f77211581426bfcd729dcbfff72360
kvnguo <kevinguo1011@gmail.com>
2023-12-01 13:57:53 -0500

    Added mouseover highlighting

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -7,11 +7,33 @@ import { geoJSONData } from '../utils/constants/geoData';
 
 function Map() {
   const knoxvillePosition: LatLngTuple = [35.9606, -83.9207]; // Latitude and Longitude of Knoxville, TN
+
   const style = () => ({
     fillColor: 'blue',
     weight: 2,
     fillOpacity: 0.1,
   });
+
+  const highlightFeature = (e: any) => {
+    const layer = e.target;
+    layer.setStyle({
+      fillColor: 'orange',
+      weight: 3,
+      fillOpacity: 0.3,
+    });
+  };
+
+  const resetHighlight = (e: any) => {
+    const layer = e.target;
+    layer.setStyle(style());
+  };
+
+  const onEachFeature = (feature: any, layer: any) => {
+    layer.on({
+      mouseover: highlightFeature,
+      mouseout: resetHighlight,
+    });
+  };
 
   return (
     <div
@@ -22,7 +44,7 @@ function Map() {
     >
       <MapContainer
         center={knoxvillePosition}
-        zoom={13}
+        zoom={10}
         style={{
           maxWidth: '500px',
           maxHeight: '500px',
@@ -32,10 +54,13 @@ function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <GeoJSON data={geoJSONData} style={style} />
+        <GeoJSON
+          data={geoJSONData}
+          style={style}
+          onEachFeature={onEachFeature}
+        />
       </MapContainer>
     </div>
   );
 }
-
 export default Map;
