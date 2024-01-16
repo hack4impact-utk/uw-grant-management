5d8d566a41ba690a53c2697337b969f7e7a972c5
LakelonBailey <lake.bailey@icloud.com>
2024-01-16 13:57:02 -0500

    added some click and hover events to the map to display zip codes

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -1,38 +1,74 @@
 'use client';
+import React, { useRef } from 'react';
 import './mapstyles.css';
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet';
-import { LatLngTuple } from 'leaflet';
+import L, { LatLngTuple, Layer, LeafletMouseEvent } from 'leaflet';
 import { geoJSONData } from '../utils/constants/geoData';
 
 function Map() {
-  const knoxvillePosition: LatLngTuple = [35.9606, -83.9207]; // Latitude and Longitude of Knoxville, TN
+  const knoxvillePosition: LatLngTuple = [35.9606, -83.9207];
+  const mapRef = useRef<L.Map | null>(null);
+
   const style = () => ({
     fillColor: 'blue',
     weight: 2,
     fillOpacity: 0.1,
   });
 
+  const highlightFeature = (e: LeafletMouseEvent) => {
+    const layer = e.target as L.Path;
+    layer.setStyle({
+      fillOpacity: 0.5,
+    });
+  };
+
+  const resetHighlight = (e: LeafletMouseEvent) => {
+    const layer = e.target as L.Path;
+    layer.setStyle({
+      fillOpacity: 0.1,
+    });
+    layer.closePopup();
+  };
+
+  const handleLayerMouseover = (e: LeafletMouseEvent, layer: Layer) => {
+    highlightFeature(e);
+    layer.openPopup(e.target.getBounds().getCenter());
+  };
+
+  const handleLayerMouseout = (e: LeafletMouseEvent, layer: Layer) => {
+    resetHighlight(e);
+    layer.closePopup();
+  };
+
+  const onEachFeature = (feature: GeoJSON.Feature, layer: Layer) => {
+    const zipCode = feature.properties?.ZCTA5CE10 as string;
+    layer.on({
+      mouseover: (e) => handleLayerMouseover(e, layer),
+      mouseout: (e) => handleLayerMouseout(e, layer),
+      click: () => alert(`Zip code: ${zipCode}`),
+    });
+
+    layer.bindPopup(zipCode);
+  };
+
   return (
-    <div
-      style={{
-        width: '100%',
-        height: '50%',
-      }}
-    >
+    <div style={{ width: '100%', height: '50%' }}>
       <MapContainer
         center={knoxvillePosition}
-        zoom={13}
-        style={{
-          maxWidth: '500px',
-          maxHeight: '500px',
-        }}
+        zoom={10}
+        style={{ maxWidth: '500px', maxHeight: '500px' }}
+        ref={mapRef}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <GeoJSON data={geoJSONData} style={style} />
+        <GeoJSON
+          data={geoJSONData}
+          style={style}
+          onEachFeature={onEachFeature}
+        />
       </MapContainer>
     </div>
   );
