e033944d5b48b0671500c817d6bf4111f7515d40
zperry4 <zactperry73@gmail.com>
2024-03-27 15:13:46 -0400

    update import form

--- src/components/ImportForm/index.tsx
+++ src/components/ImportForm/index.tsx
@@ -1,37 +1,219 @@
 'use client';
 import { useState } from 'react';
-import { Box, Button, TextField } from '@mui/material';
+import { validateImportForm } from '@/utils/validation/importFormValidation';
+import { Box, Button, TextField, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
-// inputs
-// 1.) Import csv file
-// 2.) reporting period start year
-// 3.) reporting period start month
-// 4.) reporting period end year
-// 5.) reporting period end month
+interface ImportFormData {
+  file: File | null;
+  startYear: string;
+  startMonth: string;
+  endYear: string;
+  endMonth: string;
+}
 
-// TODO
-// For importing I need to do a few things
-// 1.) Need to construct the correct file name based on the inputs and save to src/data/reports (probably going to have to make an API endpoint for this)
-// 2.) Once that is done, can pass it to the import function in utils
-// 3.) Enforce inputs are correct and required (check year format, check month format, check if file is uploaded, etc.)
+export default function ImportForm() {
+  const [formData, setFormData] = useState<ImportFormData>(
+    {} as ImportFormData
+  );
+  const [validationErrors, setValidationErrors] = useState({
+    file: '',
+    startYear: '',
+    startMonth: '',
+    endYear: '',
+    endMonth: '',
+  });
+  const [fileName, setFileName] = useState('');
+  const [submissionSuccess, setSubmissionSuccess] = useState(false);
+  const [submissionError, setSubmissionError] = useState(false);
 
-// all months in lowercase
-const months = [
-  'january',
-  'february',
-  'march',
-  'april',
-  'may',
-  'june',
-  'july',
-  'august',
-  'september',
-  'october',
-  'november',
-  'december',
-];
+  // Handle input change for each form field. If the field is a file, also set the file name.
+  const handleInputChange = (
+    field: keyof ImportFormData,
+    value: File | string
+  ) => {
+    setFormData((prevFormData) => ({ ...prevFormData, [field]: value }));
+    setValidationErrors((prevErrors) => ({ ...prevErrors, [field]: '' }));
+
+    // If setting the file, also set the file name.
+    if (field === 'file' && value instanceof File) {
+      setFileName(value.name);
+    }
+  };
+
+  // Handle form submission.
+  const submitForm = () => {
+    setSubmissionSuccess(false);
+    setSubmissionError(false);
+
+    const errors = validateImportForm(formData);
+
+    // Validate form inputs by checking if there are any errors.
+    if (Object.values(errors).some((error) => error !== '')) {
+      setValidationErrors(errors);
+      console.log('ERRORS: ', errors);
+      return;
+    }
+
+    // Prepare form data for submission.
+    const formInputData = new FormData();
+    formInputData.append('file', formData.file as File);
+    formInputData.append('startYear', formData.startYear as string);
+    formInputData.append('startMonth', formData.startMonth as string);
+    formInputData.append('endYear', formData.endYear as string);
+    formInputData.append('endMonth', formData.endMonth as string);
+
+    // API Call to submit the form data.
+    fetch('/api/import', {
+      method: 'POST',
+      body: formInputData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          setSubmissionSuccess(true);
+
+          setFormData({
+            file: null,
+            startYear: '',
+            startMonth: '',
+            endYear: '',
+            endMonth: '',
+          });
+          setFileName('');
+
+          // Set timeout just to show the success message for a few seconds.
+          setTimeout(() => {
+            setSubmissionSuccess(false);
+          }, 5000);
+        } else {
+          console.log('Error on submission: ', data.message);
+          setSubmissionError(true);
+        }
+      })
+      .catch((error) => {
+        console.error('Error on submission: ', error);
+        setSubmissionError(true);
+      });
+  };
+
+  return (
+    <OuterBox>
+      <Box
+        component="form"
+        autoComplete="off"
+        flexDirection="column"
+        p={2}
+        sx={{ border: '2px blue' }}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Typography variant="h3">Import Form</Typography>
+        <Typography variant="subtitle1">
+          Please upload a CSV file to import data to the database.
+        </Typography>
+        <br />
+        <Button
+          variant="contained"
+          component="label"
+          startIcon={<CloudUploadIcon />}
+          style={{}}
+        >
+          Upload File
+          <VisuallyHiddenInput
+            accept=".csv"
+            id="contained-button-file"
+            type="file"
+            onChange={(e) => handleInputChange('file', e.target.files[0])}
+            required
+          />
+        </Button>
+        {validationErrors.file && (
+          <Typography variant="subtitle1" style={{ color: 'red' }}>
+            {validationErrors.file}
+          </Typography>
+        )}
+        {{ fileName } && (
+          <Typography variant="subtitle1">{fileName}</Typography>
+        )}
+        <br />
+        <TextField
+          label="Reporting Period Start Year"
+          placeholder="YYYY"
+          variant="outlined"
+          margin="normal"
+          onChange={(e) => handleInputChange('startYear', e.target.value)}
+          value={formData.startYear || ''}
+          error={!!validationErrors.startYear}
+          helperText={validationErrors.startYear}
+          style={{ width: '75%' }}
+          required
+        />
+
+        <TextField
+          label="Reporting Period Start Month"
+          placeholder="Month"
+          variant="outlined"
+          margin="normal"
+          onChange={(e) => handleInputChange('startMonth', e.target.value)}
+          error={!!validationErrors.startMonth}
+          helperText={validationErrors.startMonth}
+          value={formData.startMonth || ''}
+          style={{ width: '75%' }}
+          required
+        />
+
+        <TextField
+          label="Reporting Period End Year"
+          placeholder="YYYY"
+          variant="outlined"
+          margin="normal"
+          onChange={(e) => handleInputChange('endYear', e.target.value)}
+          error={!!validationErrors.endYear}
+          helperText={validationErrors.endYear}
+          value={formData.endYear || ''}
+          style={{ width: '75%' }}
+          required
+        />
+
+        <TextField
+          label="Reporting Period End Month"
+          placeholder="Month"
+          variant="outlined"
+          margin="normal"
+          onChange={(e) => handleInputChange('endMonth', e.target.value)}
+          error={!!validationErrors.endMonth}
+          helperText={validationErrors.endMonth}
+          value={formData.endMonth || ''}
+          style={{ width: '75%' }}
+          required
+        />
+        <br />
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            submitForm();
+          }}
+        >
+          Submit
+        </Button>
+        {submissionSuccess && (
+          <Typography variant="subtitle1" style={{ color: 'green' }}>
+            Upload Successful
+          </Typography>
+        )}
+        {submissionError && (
+          <Typography variant="subtitle1" style={{ color: 'red' }}>
+            Error on submission. Please try again...
+          </Typography>
+        )}
+      </Box>
+    </OuterBox>
+  );
+}
 
 const VisuallyHiddenInput = styled('input')({
   clip: 'rect(0 0 0 0)',
@@ -45,209 +227,10 @@ const VisuallyHiddenInput = styled('input')({
   width: 1,
 });
 
-export default function ImportForm() {
-  const [file, setFile] = useState(null);
-
-  const [startYear, setStartYear] = useState(null);
-  const [startYearError, setStartYearError] = useState(false);
-
-  const [startMonth, setStartMonth] = useState(null);
-  const [startMonthError, setStartMonthError] = useState(false);
-
-  const [endYear, setEndYear] = useState(null);
-  const [endYearError, setEndYearError] = useState(false);
-
-  const [endMonth, setEndMonth] = useState(null);
-  const [endMonthError, setEndMonthError] = useState(false);
-
-  // Right now, just grabbing the file and saving it to state
-  const handleFileChange = (e) => {
-    setFile(e.target.value);
-    console.log('Name: ', e.target.value);
-
-    // Just get the csv name
-    const fileName = e.target.value.split('\\').pop();
-    setFile(fileName);
-
-    // All file reading stuff, may be handled somewhere else
-    const file = e.target.files[0];
-    const reader = new FileReader();
-    reader.onload = (event) => {
-      const fileContent = event?.target?.result;
-      console.log('Contents: ', fileContent);
-    };
-    reader.readAsText(file);
-  };
-
-  // Handle start year validation
-  const handleStartYearChange = (e) => {
-    const year = e.target.value;
-
-    if (year.length !== 4) {
-      setStartYearError(true);
-    } else {
-      setStartYearError(false);
-      setStartYear(e.target.value);
-      console.log('Start Year: ', startYear);
-    }
-    return;
-  };
-  // Handle start month validation
-  const handleStartMonthChange = (e) => {
-    const input_month = e.target.value.toLowerCase();
-
-    if (months.includes(input_month)) {
-      setStartMonthError(false);
-      setStartMonth(input_month);
-      console.log('Start Month: ', startMonth);
-    } else {
-      setStartMonthError(true);
-    }
-    return;
-  };
-
-  // Handle end year validation
-  const handleEndYearChange = (e) => {
-    const year = e.target.value;
-
-    if (year.length !== 4) {
-      setEndYearError(true);
-    } else {
-      setEndYearError(false);
-      setEndYear(e.target.value);
-      console.log('End Year: ', endYear);
-    }
-    return;
-  };
-
-  // Handle end month validation
-  const handleEndMonthChange = (e) => {
-    const input_month = e.target.value.toLowerCase();
-
-    if (months.includes(input_month)) {
-      setEndMonthError(false);
-      setEndMonth(input_month);
-      console.log('End Month: ', endMonth);
-    } else {
-      setEndMonthError(true);
-    }
-    return;
-  };
-
-  const submitForm = () => {
-    console.log('Submitting form');
-    const newFileName = `${startYear}_${startMonth}_${endYear}_${endMonth}.csv`;
-    console.log('New File Name: ', newFileName);
-
-    // Save the file to src/data/reports
-    // Call import function on the new file
-    // If successful, display success message
-    // If not, display error message
-    // Clear the form
-  };
-
-  return (
-    <Box
-      component="form"
-      autoComplete="off"
-      alignItems="center"
-      gap={4}
-      p={2}
-      sx={{ border: '2px blue' }}
-    >
-      <h1>Import Form</h1>
-      <br />
-      <div>
-        <Button
-          variant="contained"
-          component="label"
-          startIcon={<CloudUploadIcon />}
-        >
-          Upload File
-          <VisuallyHiddenInput
-            accept=".csv"
-            id="contained-button-file"
-            type="file"
-            onChange={handleFileChange}
-            required
-          />
-        </Button>
-        {file && <p>File: {file}</p>}
-      </div>
-      <br />
-
-      <TextField
-        label="Reporting Period Start Year"
-        placeholder="YYYY"
-        variant="outlined"
-        margin="normal"
-        error={startYearError}
-        helperText={
-          startYearError ? 'Invalid year format. Please use YYYY format.' : ''
-        }
-        onBlur={handleStartYearChange}
-        onChange={handleStartYearChange}
-        fullWidth
-        required
-      />
-      <br />
-      <TextField
-        label="Reporting Period Start Month"
-        placeholder="Month"
-        variant="outlined"
-        margin="normal"
-        error={startMonthError}
-        helperText={
-          startMonthError
-            ? 'Invalid month format. Please enter full month name.'
-            : ''
-        }
-        onBlur={handleStartMonthChange}
-        onChange={handleStartMonthChange}
-        fullWidth
-        required
-      />
-      <br />
-      <TextField
-        label="Reporting Period End Year"
-        placeholder="YYYY"
-        variant="outlined"
-        margin="normal"
-        error={endYearError}
-        helperText={
-          endYearError ? 'Invalid year format. Please use YYYY format.' : ''
-        }
-        onBlur={handleEndYearChange}
-        onChange={handleEndYearChange}
-        fullWidth
-        required
-      />
-      <br />
-      <TextField
-        label="Reporting Period End Month"
-        variant="outlined"
-        margin="normal"
-        error={endMonthError}
-        helperText={
-          endMonthError
-            ? 'Invalid month format. Please enter full month name.'
-            : ''
-        }
-        onBlur={handleEndMonthChange}
-        onChange={handleEndMonthChange}
-        fullWidth
-        required
-      />
-      <br />
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => {
-          submitForm();
-        }}
-      >
-        Submit
-      </Button>
-    </Box>
-  );
-}
+const OuterBox = styled(Box)({
+  backgroundColor: '#f0f0f0',
+  padding: '15px',
+  margin: 'auto',
+  maxWidth: '600px',
+  marginTop: '20px',
+});
